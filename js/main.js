a5ec63157b7b83eac8520712c183ba14f52ed774
mogamoga <mogamoga.g1thu6@gmail.com>
2022-05-05 18:56:10 +0900

    ぱすかる！

--- js/main.js
+++ js/main.js
@@ -5,9 +5,38 @@ const viewModel = {
     },
     data() {
         return {
-            pascalsTriangle: [
-                [1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]
-            ]
+            pascalsTriangle: [[]]
+        }
+    },
+    created() {
+        this.initPascalsTriangle(30);
+    },
+    methods: {
+        initPascalsTriangle(colCount) {
+            const baseNum = 1;
+            let prevNums = [baseNum];
+            this.pascalsTriangle = [prevNums];
+            
+            if (colCount <= 1) {
+                return;
+            }
+
+            for (let i = 1; i < colCount; i++) {
+                const nums = [baseNum];
+                for (let j = 0; j < i; j++) {
+                    if (j === i - 1) {
+                        nums.push(baseNum);
+                    }
+                    else {
+                        const num = (prevNums[j] + prevNums[j + 1]) % 2;
+                        nums.push(num);
+                    }
+                }
+                this.pascalsTriangle.push(nums);
+                prevNums = nums;
+            }
+
+            return;
         }
     }
 };
